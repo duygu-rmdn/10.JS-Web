3433bbeb75b277fded18188b391fdf6a40eba0df
Duygu <duygumetin99@gmail.com>
2023-01-23 17:26:05 +0200

    config handlebars

--- JS Back-End/04.Workshop/src/index.js
+++ JS Back-End/04.Workshop/src/index.js
@@ -1,1 +1,13 @@
-console.log('Initial');
+const express = require('express');
+
+const config = require('./config');
+const setupViewEngine = require('./config/viewEngine');
+
+const app = express();
+setupViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(config.PORT , console.log(`Server is listening on port: ${config.PORT}`));
