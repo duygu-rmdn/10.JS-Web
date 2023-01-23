6e8a4a0e85f1dcf37d463fc9ba239c65f3577cba
Duygu <duygumetin99@gmail.com>
2023-01-23 20:46:18 +0200

    init homeController

--- JS Back-End/04.Workshop/src/index.js
+++ JS Back-End/04.Workshop/src/index.js
@@ -8,6 +8,6 @@ const app = express();
 setupViewEngine(app);
 
 app.use(express.static('src/public'));
-app.use(routes);    
+app.use(routes);
 
-app.listen(config.PORT, console.log(`Server is listening on port: ${config.PORT}`));
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
