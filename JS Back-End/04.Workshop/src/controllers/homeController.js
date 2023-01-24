d10e80348acba551d90b8957588df79cc02a0a65
Duygu <duygumetin99@gmail.com>
2023-01-24 15:01:05 +0200

    details and 404 page - done

--- JS Back-End/04.Workshop/src/controllers/homeController.js
+++ JS Back-End/04.Workshop/src/controllers/homeController.js
@@ -7,3 +7,7 @@ exports.getHomePage = (req, res) => {
 exports.getAboutPage = (req, res) => {
     res.render('about');
 };
+
+exports.getNotFoundPage = (req, res) => {
+    res.render('404');
+};
--- JS Back-End/04.Workshop/src/routes.js
+++ JS Back-End/04.Workshop/src/routes.js
@@ -5,8 +5,14 @@ const homeController = require('./controllers/homeController');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
+router.get('/404', homeController.getNotFoundPage);
 
 router.get('/create', cubeController.getCreateCube);
 router.post('/create', cubeController.postCreateCube);
+router.get('/details/:cubeId', cubeController.getDetails);
+
+router.get('/*', homeController.getNotFoundPage);
+
+
 
 module.exports = router;
