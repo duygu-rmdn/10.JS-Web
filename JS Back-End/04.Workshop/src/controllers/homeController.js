0661944eb7729fb0c0c2b4d81bdd2c8f30407ddb
Duygu <duygumetin99@gmail.com>
2023-01-24 15:19:07 +0200

    Add search

--- JS Back-End/04.Workshop/src/controllers/homeController.js
+++ JS Back-End/04.Workshop/src/controllers/homeController.js
@@ -1,7 +1,22 @@
 const db = require('../db.json')
 
 exports.getHomePage = (req, res) => {
-    res.render('index', { cubes: db.cubes });
+    let cubes = db.cubes;
+    const {search, from, to} = req.query;
+
+    if (search) {
+        cubes = cubes.filter(cube => cube.name.toLowerCase().includes(search.toLowerCase()));
+    }
+
+    if (from) {
+        cubes = cubes.filter(cube => cube.difficultyLevel >= from);
+    }
+
+    if (to) {
+        cubes = cubes.filter(cube => cube.difficultyLevel <= to);
+    }
+
+    res.render('index', { cubes, search, from, to });
 };
 
 exports.getAboutPage = (req, res) => {
