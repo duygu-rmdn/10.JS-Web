2d97350819f3b79477a3ef7cad1e34ceaa4e8554
Duygu <duygumetin99@gmail.com>
2023-01-31 23:36:34 +0200

    accessory controller

--- JS Back-End/04.06.Workshop/src/routes.js
+++ JS Back-End/04.06.Workshop/src/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController');
 
 router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
@@ -10,6 +11,8 @@ router.get('/404', homeController.getNotFoundPage);
 router.get('/create', cubeController.getCreateCube);
 router.post('/create', cubeController.postCreateCube);
 router.get('/details/:cubeId', cubeController.getDetails);
+
+router.use('/accessory', accessoryController);
 
 router.get('/*', homeController.getNotFoundPage);
 
