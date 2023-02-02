1b0ac19405abb589a6139191d7363ff1c7cef422
Duygu <duygumetin99@gmail.com>
2023-02-02 20:51:09 +0200

    Demo for sessions

--- JS Back-End/07.Session and Authentication/cookies/index.js
+++ JS Back-End/07.Session and Authentication/cookies/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
 
 const app = express();
 
-app.use(express.urlencoded( {extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
 app.get('/', (req, res) => {
     res.send(`<h1>Home page</h1>
@@ -28,29 +35,34 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
-    if(username == 'Duygu' && password == 'rmdn'){
+    if (username == 'Duygu' && password == 'rmdn') {
         const authData = {
             username: 'DUDU'
         };
 
         res.cookie('auth', JSON.stringify(authData));
+        req.session.username = 'Duygu';
+        req.session.privInfo = 'privInfo privInfoprivInfo';
+
         return res.redirect('/');
     }
 
     res.status(401).end();
 });
 
-app.get('/profile', (req, res) =>{
+app.get('/profile', (req, res) => {
     const authData = req.cookies['auth'];
 
     if (!authData) {
         return res.status(401).end();
     }
- 
-    const {username} = JSON.parse(authData);
 
+    const { username } = JSON.parse(authData);
+
+    console.log(req.session);
+    
     res.send(`
     <h2> Hello - ${username}<?h2>
         `);
