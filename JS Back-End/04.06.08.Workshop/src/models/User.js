e4b037e549709d39b1cbd395a49d95b1bc46ce67
Duygu <duygumetin99@gmail.com>
2023-02-05 18:25:01 +0200

    Register function done

---
+++ JS Back-End/04.06.08.Workshop/src/models/User.js
@@ -0,0 +1,31 @@
+const {Schema, model} = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        minLength: 3 
+    },
+    password:{
+        type: String,
+        required: true,
+        minLength: [6, 'Password is too short']
+    }
+});
+
+userSchema.pre('save', function(next){
+    bcrypt.hash(this.password, 10)
+    .then(hash => {
+        this.password = hash;   
+        next();
+    });
+
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
+
+
+
