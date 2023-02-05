6f4284f4901b2232e3a1ceba71a8ffb69d669247
Duygu <duygumetin99@gmail.com>
2023-02-05 18:56:08 +0200

    login and passwd check function done

--- JS Back-End/04.06.08.Workshop/src/models/User.js
+++ JS Back-End/04.06.08.Workshop/src/models/User.js
@@ -23,6 +23,10 @@ userSchema.pre('save', function(next){
 
 });
 
+userSchema.method('validatePassword', function(password) {
+    return bcrypt.compare(password, this.password);
+});
+
 const User = model('User', userSchema);
 
 module.exports = User;
--- JS Back-End/04.06.08.Workshop/src/service/authService.js
+++ JS Back-End/04.06.08.Workshop/src/service/authService.js
@@ -3,3 +3,15 @@ const User = require('../models/User');
 exports.getUserByUsername = (username) => User.findOne({username});
 
 exports.register = (username, password) => User.create({username, password});
+
+exports.login = async (username, password) => {
+    const user = await this.getUserByUsername(username);
+
+    const isValidPassword = await user.validatePassword(password);
+    if(!user || !isValidPassword){
+        throw 'Invalid username or password';
+    }
+
+    return user;
+}
+
