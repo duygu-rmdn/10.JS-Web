a3ac673b46d3344440b3d4f72e904fc8612163b7
Duygu <duygumetin99@gmail.com>
2023-02-05 12:39:36 +0200

    Add new resurses

---
+++ JS Back-End/04.06.08.Workshop/src/routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const cubeController = require('./controllers/cubeController');
+const homeController = require('./controllers/homeController');
+const accessoryController = require('./controllers/accessoryController');
+
+router.get('/', homeController.getHomePage);
+router.get('/about', homeController.getAboutPage);
+router.get('/404', homeController.getNotFoundPage);
+
+router.get('/cubes/create', cubeController.getCreateCube);
+router.post('/cubes/create', cubeController.postCreateCube);
+router.get('/cubes/:cubeId/details', cubeController.getDetails);
+router.get('/cubes/:cubeId/attach', cubeController.getAttachAccessory);
+router.post('/cubes/:cubeId/attach', cubeController.postAttachAccessory);
+
+router.use('/accessories', accessoryController);
+
+router.get('/*', homeController.getNotFoundPage);
+
+
+
+module.exports = router;
