69fe6649cb902439b98865f19ec1e6c392236d58
Duygu <duygumetin99@gmail.com>
2023-02-16 09:59:58 +0200

    Correct port

--- JS Back-End/10.Exam Preparation/ExpressApp/index.js
+++ JS Back-End/10.Exam Preparation/ExpressApp/index.js
@@ -22,4 +22,4 @@ mongoose.set('strictQuery', false);
 //TODO: change the name of db
 mongoose.connect('mongodb://127.0.0.1:27017/crypto')
 
-app.listen('5000', () => console.log('Server is running on port 5000...'));
+app.listen('3000', () => console.log('Server is running on port 3000...'));
