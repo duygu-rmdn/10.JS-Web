88c7a2090a331d18b8364111b1d3b1ef51113759
Duygu <duygumetin99@gmail.com>
2023-03-13 21:46:27 +0200

    Set architecture and start create games

---
+++ ReactJs/07.Workshop Routing/gamesplay/src/App.js
@@ -0,0 +1,44 @@
+import { Route, Routes} from 'react-router-dom';
+import { useEffect, useState,  } from 'react';
+import * as gameService from './services/gameService';
+import { Header } from "./components/Header/Header";
+import { Catalog } from "./components/Catalog/Catalog";
+import { Home } from "./components/Home/Home";
+import { Create } from "./components/CreateGame/CreateGame";
+import { Details } from "./components/Details/Details";
+import { Edit } from "./components/Edit/Edit";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+
+function App() {
+    const [games, setGames] = useState([]);
+
+    useEffect(() => {
+        gameService.getAll()
+        .then(result => setGames(result));
+    }, []);
+
+    const onCreateGameSubmit = (data) => {
+        console.log(data);
+    } 
+    
+    return (
+        <div id="box">
+            <Header />
+            <main id="main-content">
+                <Routes> 
+                    <Route path='/' element={<Home/>} />
+                    <Route path='/catalog' element={<Catalog games={games}/>} />
+                    <Route path='/create' element={<Create onCreateGameSubmit={onCreateGameSubmit}/>} />
+                    <Route path='/details' element={<Details/>} />
+                    <Route path='/edit' element={<Edit/>} />
+                    <Route path='/login' element={<Login/>} />
+                    <Route path='/register' element={<Register/>} />
+                </Routes>
+            </main>
+
+        </div>
+    );
+}
+
+export default App;
